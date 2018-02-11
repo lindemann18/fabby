44ecbe2960453ce754c211f934b3de3951412a58
Chuvakovich Zukovsky <juliusbelmont_1@hotmail.com>
2018-02-11 16:15:28 -0600

    feature: defined the structure for the router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import HeaderContainer from './general/containers/header-container';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Switch,Route} from 'react-router-dom';
+import MainContainer from './pages/containers/main-container';
+import AboutContainer from './pages/containers/about-container';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <HeaderContainer />
+          <Switch>
+            <Route path="/about" component={AboutContainer} />
+            <Route path="/" component={MainContainer} />
+      </Switch>
       </div>
     );
   }
