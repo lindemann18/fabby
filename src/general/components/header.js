33de81f61bc6ada8b3d102cc03d9771c4155e53e
Chuvakovich Zukovsky <juliusbelmont_1@hotmail.com>
2018-02-18 16:58:26 -0600

    feature: defined more styles aboutthe hader

--- src/general/components/header.js
+++ src/general/components/header.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = ()=>(
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+  <nav className="navbar navbar-expand-lg navbar-light">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <Link to="/" className="nav-link">Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/aboutme" className="nav-link">¿Quien Soy?</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/about" className="nav-link">About</Link>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+    <div className="collapse navbar-collapse justify-content-end menu" id="navbarSupportedContent">
+      <div className="logo">
+        <Link to="/" className="logo__name">Dra. Faviola Sanchez</Link>
+      </div>
+      <ul className="navbar-nav nav">
+        <li className="nav-item active">
+          <Link to="/" className="nav-link">Inicio</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/aboutme" className="nav-link">¿Quien Soy?</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">Servicios</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">Testimonios</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">Contacto</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 )
 
 export default Header;
