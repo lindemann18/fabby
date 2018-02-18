9d0b64cc57e6d3f6d7c321148edc5100495a27e1
Chuvakovich Zukovsky <juliusbelmont_1@hotmail.com>
2018-02-18 14:23:44 -0600

    feature: defined the structure and some styles for te aboutme

--- src/general/components/header.js
+++ src/general/components/header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = ()=>(
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="">Navbar</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -20,7 +20,7 @@ const Header = ()=>(
         <Link to="/about" className="nav-link">About</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
+        <a className="nav-link disabled" href="">Disabled</a>
       </li>
     </ul>
   </div>
--- src/pages/components/aboutme.js
+++ src/pages/components/aboutme.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Fabby from '../../img/fabby.jpg';
 
 const AboutMe = ()=> (
     <div className="aboutme col-md-12">
         <h1 className="aboutme__name">Dra. Faviola Sanchez</h1>
+        <div className="aboutme__picture_container">
+          <img src={ Fabby } className="rounded-circle aboutme__picture" alt="dra fabiola sanchez" />
+        </div>
         <p className="aboutme__title">PROFESORA, INSTITUTO DE DESARROLLO HUMANO E
           INVESTIGACION IDHEI</p>
 
@@ -15,13 +19,41 @@ const AboutMe = ()=> (
         </p>
 
         <div className="experience col-md-12">
-            <div className="experience__job col-md-4">
+            <div className="experience__job --first col-md-5 col-xs-12">
                   <h3 className="experience__job_title">Tutora</h3>
                   <span className="experience__job_place">Universidad Autónoma De Sinaloa</span>
-                  <span className="date">2010 - A la fecha</span>
-                  <p clasName="experience__job_descriptoin">
+                  <span className="experience__date">2010 - A la fecha</span>
+                  <p className="experience__job_description">
                     APOYO A ALUMNOS Y DOCENTES EN DIVERSAS NECESIDADES EDUCATIVAS
                     ENTRENAMIENTO A DOCENTES
+                  </p>
+            </div>
+
+            <div className="experience__job col-md-5 col-xs-12">
+                  <h3 className="experience__job_title">Profesora</h3>
+                  <span className="experience__job_place">Instituto De Desarrollo Humano E Investigación IDHEI</span>
+                  <span className="experience__date">2008 - A la fecha</span>
+                  <p className="experience__job_description">
+                    FORMADORA DE VARIAS GENERACIONES A NIVEL MAESTRIA Y DOCTORADO EN DESARROLLO HUMANO
+                  </p>
+            </div>
+
+            <div className="experience__job --first col-md-5 col-xs-12">
+                  <h3 className="experience__job_title">Profesora</h3>
+                  <span className="experience__job_place">Instituto Humanista DE Sinaloa</span>
+                  <span className="experience__date">2004 - 2006</span>
+                  <p className="experience__job_description">
+                    FORMADORA DE VARIAS GENERACIONES A NIVEL MAESTRIA Y DOCTORADO EN DESARROLLO HUMANO
+                  </p>
+            </div>
+
+            <div className="experience__job col-md-5 col-xs-12">
+                  <h3 className="experience__job_title">Maestra</h3>
+                  <span className="experience__job_place">Facultad DE Contaduría Y Administración, UAS</span>
+                  <span className="experience__date">1985 - A la fecha</span>
+                  <p className="experience__job_description">
+                    COAUTORA DEL PLAN DE ESTUDIOS V, LIC. EN CONTADURIA PUBLICA, ADMINISTRACCION DE EMPRESAS Y NEGOCIOS Y COMERCIO INTERNACIONALES.
+                    AUTORA DE LOS PROGRAMAS EN LAS MATERIAS DE DESARROLLO HUMANO, DESARROLLO DE HABILIDADES CREATIVAS, Y ESTRATEGIAS DE APRENDIZAJE
                   </p>
             </div>
         </div>
