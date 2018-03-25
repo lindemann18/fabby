9c80c2bf5febaad93f0f9022fa02f0af354c9928
lindemann18 <juliusbelmont_1@hotmail.com>
2018-03-24 19:49:46 -0600

    defined the structure for testimonials

---
+++ src/pages/components/testimonial.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Testimonial = ()=>(
+  <div className="Testimonial col-md-12">
+        <div className="Testimonial__header col-md-8 offset-md-2">
+            <h3 className="Testimonial__title">Testimonios</h3>
+
+        </div>
+
+        <div className="Testimonial__aside col-md-5">
+          <p className="Testimonial__headtext">Todos los pacientes que han hecho terapia con BrainGym han tenido cambios
+          significativos en sus vidas.</p>
+
+          <p className="Testimonial__explanation">
+            La doctora Fabiola Sánchez ha trabajado con una gran cantidad de pacientes, instituciones
+            educativas y privadas para fomentar Brain Gym como una forma alternativa de terapia
+            para mejorar la vida de las personas...
+          </p>
+        </div>
+
+        <div className="Testimonial__body col-md-7 pull-right">
+          <div className="testimonial-component">
+            <p>“Sales coaches will be able to lead more reps and provide more
+              focused and valuable coaching through the use of automated sales
+               analytics from companies like ClearSlide.”</p>
+               <span class="testimonial-component__name ">Chuvakovich Zukosky</span>
+          </div>
+
+          <div className="testimonial-component">
+            <p>“Sales coaches will be able to lead more reps and provide more
+              focused and valuable coaching through the use of automated sales
+               analytics from companies like ClearSlide.”</p>
+               <span class="testimonial-component__name ">Chuvakovich Zukosky</span>
+          </div>
+
+          <div className="testimonial-component">
+            <p>“Sales coaches will be able to lead more reps and provide more
+              focused and valuable coaching through the use of automated sales
+               analytics from companies like ClearSlide.”</p>
+               <span class="testimonial-component__name ">Chuvakovich Zukosky</span>
+          </div>
+
+
+        </div>
+  </div>
+)
+
+export default Testimonial;
