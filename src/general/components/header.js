aea3cd2514a52aeed6bb8ad5b2d267f9d3eb7e29
lindemann18 <juliusbelmont_1@hotmail.com>
2018-03-25 19:20:16 -0600

    defined the active and hover properties

--- src/general/components/header.js
+++ src/general/components/header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = ()=>(
   <div>
     <div className="logo__mobile">
-      <Link to="/" className="logo__name">Dra. Faviola Sanchez</Link>
+      <span  className="logo__name">Dra. Faviola Sanchez</span>
     </div>
     <nav className="navbar navbar-expand-lg navbar-light">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,21 +13,21 @@ const Header = ()=>(
 
       <div className="collapse navbar-collapse justify-content-end menu" id="navbarSupportedContent">
         <div className="logo">
-          <Link to="/" className="logo__name">Dra. Faviola Sanchez</Link>
+          <span className="logo__name">Dra. Faviola Sanchez</span>
         </div>
         <ul className="navbar-nav nav">
-          <li className="nav-item active">
-            <Link to="/" className="nav-link">Inicio</Link>
+          <li className="nav-item">
+            <NavLink exact to="/" className="nav-link">Inicio</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/aboutme" className="nav-link">¿Quien Soy?</Link>
+            <NavLink to="/aboutme" className="nav-link">¿Quien Soy?</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-link">Servicios</Link>
+            <NavLink to="/about" className="nav-link">Servicios</NavLink>
           </li>
 
           <li className="nav-item">
-            <Link to="/testimonial" className="nav-link">Testimonios</Link>
+            <NavLink to="/testimonial" className="nav-link">Testimonios</NavLink>
           </li>
 
           <li className="nav-item">
